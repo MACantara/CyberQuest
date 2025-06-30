4f6e2542a363628245aa9660c9db70900ee5cd4a
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2025-07-01 01:08:32 +0800

    Add level-one dialogues for easy importing

---
+++ app/static/js/simulated-pc/dialogues/levels/level-one/index.js
@@ -0,0 +1,7 @@
+// Export all level-one dialogues for easy importing
+export { TutorialDialogue } from './tutorial-dialogue.js';
+export { Challenge1Dialogue } from './challenge1-dialogue.js';
+export { Challenge2Dialogue } from './challenge2-dialogue.js';
+export { Challenge3Dialogue } from './challenge3-dialogue.js';
+export { Challenge4Dialogue } from './challenge4-dialogue.js';
+export { LevelCompletionDialogue } from './level-completion-dialogue.js';
