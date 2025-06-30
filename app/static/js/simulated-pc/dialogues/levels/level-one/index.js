d8b520296f944694e8ed583f0cf6fe6408275c3c
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2025-07-01 01:32:13 +0800

    Remove TutorialDialogue export to streamline level-one dialogue management

--- app/static/js/simulated-pc/dialogues/levels/level-one/index.js
+++ app/static/js/simulated-pc/dialogues/levels/level-one/index.js
@@ -1,5 +1,4 @@
 // Export all level-one dialogues for easy importing
-export { TutorialDialogue } from './tutorial-dialogue.js';
 export { Challenge1Dialogue } from './challenge1-dialogue.js';
 export { Challenge2Dialogue } from './challenge2-dialogue.js';
 export { Challenge3Dialogue } from './challenge3-dialogue.js';
