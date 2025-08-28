eec61fc81974a72378f40d1055535852f80c0658
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-08-29 00:34:06 +0800

    Add central export files for Level 2 data and dialogue modules in "Shadow in the Inbox"

---
+++ app/static/js/simulated-pc/levels/level-three/data/index.js
@@ -0,0 +1,12 @@
+/**
+ * Level 3: Malware Mayhem - Data exports
+ * Central export file for all Level 3 data modules
+ */
+
+// Export placeholder - Level 3 data modules will be added here when created
+export const level3DataLoaded = true;
+
+// Future exports will include:
+// export { malwareDatabase } from './malware-database.js';
+// export { systemProcesses } from './system-processes.js';
+// export { quarantinedFiles } from './quarantined-files.js';
---
+++ app/static/js/simulated-pc/levels/level-two/data/index.js
@@ -0,0 +1,12 @@
+/**
+ * Level 2: Shadow in the Inbox - Data exports
+ * Central export file for all Level 2 data modules
+ */
+
+// Export placeholder - Level 2 data modules will be added here when created
+export const level2DataLoaded = true;
+
+// Future exports will include:
+// export { phishingEmails } from './phishing-emails.js';
+// export { emailTemplates } from './email-templates.js';
+// export { securityAlerts } from './security-alerts.js';
