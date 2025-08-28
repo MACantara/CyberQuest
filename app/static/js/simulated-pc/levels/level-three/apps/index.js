f8907baad02d5bb82507704b25dec304ba07f4bb
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2025-08-29 00:39:15 +0800

    Add central export file for Level 3 application modules in "Malware Mayhem"

---
+++ app/static/js/simulated-pc/levels/level-three/apps/index.js
@@ -0,0 +1,12 @@
+/**
+ * Level 3: Malware Mayhem - Apps exports
+ * Central export file for all Level 3 application modules
+ */
+
+// Export placeholder - Level 3 app modules will be added here when created
+export const level3AppsLoaded = true;
+
+// Future exports will include:
+// export { malwareScannerConfig } from './malware-scanner-config.js';
+// export { processMonitorConfig } from './process-monitor-config.js';
+// export { systemLogsConfig } from './system-logs-config.js';
